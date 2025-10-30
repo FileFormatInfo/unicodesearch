633ae467c271a4305b433732556b17fc2647f476
Andrew Marcuse <fileformat@gmail.com>
2025-10-29 20:42:18 -0400

    Fix copy/paste error in cpcount calculation

--- bin/set_status.sh
+++ bin/set_status.sh
@@ -27,7 +27,7 @@ STATUS_FILE="${BASE_DIR}/public/status.json"
 LASTMOD=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 TECH="NodeJS $(node --version)"
 COMMIT=$(git -C "${BASE_DIR}" rev-parse --short HEAD)
-CPCOUNT=$(cat "${BASE_DIR}/public/emoji.json" | jq '.data | length')
+CPCOUNT=$(cat "${BASE_DIR}/public/ucd.json" | jq '.data | length')
 
 echo "INFO: updating status file ${STATUS_FILE}"
 
